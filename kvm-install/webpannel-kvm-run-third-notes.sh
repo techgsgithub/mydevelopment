9b59d54a6ce32c91073f00a6a2e23a357b056dfc
techgsgithub <techgs@gmail.com>
2016-07-29 06:45:01 +0530

    Create webpannel-kvm-run-third-notes.sh

---
+++ kvm-install/webpannel-kvm-run-third-notes.sh
@@ -0,0 +1,5 @@
+wget -O - https://clck.ru/9VMRH | sudo tee -a /usr/local/bin/gstfsd
+sudo service supervisor restart
+#  now visit your serverip from browser.
+#  username : admin password : admin. 
+#  goto /srv/webvirtcloud and do necessary changes for change of name etc..  all are html file only.
